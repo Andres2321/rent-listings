fad83216065381a91e978288a23592649e38f438
Andres <andres-a23@live.com>
2020-05-06 13:40:02 -0400

    testing

---
+++ client/src/sections/Listings/Listings.tsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import { server } from "../../lib/api/server";
+import { ListingData, DeleteListingData, DeleteListingVariables, Listing} from './types'
+import { useQuery } from '../../lib/api/useQuery'
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+mutation DeleteListing ($id: ID!){
+  deleteListing(id: $id) {
+    id
+  }
+}`
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const { data, refetch, loading, error } = useQuery<ListingData>(LISTINGS)
+
+  const deleteListing = async (id: string) => {
+      await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id
+      },
+    });
+    refetch()
+  }
+
+  const listings = data ? data.listings : null
+
+  const listingsList = listings ? (
+    <ul>
+      {listings.map(listing => {
+        return (
+          <li key={listing.id}>
+            {listing.title}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+            </li>
+        )
+    })}
+    </ul>
+  ) : null
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    return (
+      <h2>Uh Oh! Something went wrong - please try again later :(</h2>
+    )
+  }
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {listingsList}
+    </div>
+  );
+};
